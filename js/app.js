27caea3a5fac9cc9b28b7312ae8ac22a10e41b61
javieryep <javieryep@users.noreply.github.com>
2020-05-13 17:41:40 -0500

    tips folder added to root

--- js/app.js
+++ js/app.js
@@ -88,9 +88,9 @@ function showSlides(n) {
     slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active-img", "");
   }
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  dots[slideIndex-1].className += " active-img";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
